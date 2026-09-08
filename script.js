1ad1fa09179939d95f39141097d194103595dfdb
Artur24911 <as24911@web.de>
2026-09-08 14:05:45 +0200

    js aufbau

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // global speichern
 
 // notitzen anzeigen lassen
-let notes = [];
+let notes = ['banana','rasen mähen'];
 
 function renderNotes(){
  //ich muss defenieren wo Sie anzuzeigen ist 
-
+let contentRef = document.getElementById('content')
  //wann werden Sie angezeigt
+contentRef.innerHTML = "";
+for (let indexNote = 0; indexNote < notes.length; indexNote++){
+    const note = notes[indexNote];
+    contentRef.innerHTML += getNoteTemplate(note);
+}
+}
+
+function getNoteTempalte(note){
+    return `<p>+ ${note}</p>`;
 }
 
 
