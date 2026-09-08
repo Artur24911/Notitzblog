c795d3c27a5053d2151e7951b881d757150c27a3
Artur24911 <as24911@web.de>
2026-09-08 12:51:45 +0200

    Neues Projekt

---
+++ script.js
@@ -0,0 +1,20 @@
+// global speichern
+
+// notitzen anzeigen lassen
+let notes = [];
+
+function renderNotes(){
+ //ich muss defenieren wo Sie anzuzeigen ist 
+
+ //wann werden Sie angezeigt
+}
+
+
+
+   
+
+// notitzen hinzufügen
+
+// notitzen löschen
+
+//notitzen archivieren
